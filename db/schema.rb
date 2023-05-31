59dd34fae4eaaf69d4ad1ef8523d97e232d46346
Billal Chami <billal7chami@gmail.com>
2023-05-31 15:53:57 +0100

    Update Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -35,14 +35,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_143620) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "full_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
