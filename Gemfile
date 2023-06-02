0819174eb7ca3a770f5210d7a71a26b1a76542cf
blase147 <solarmails2@gmail.com>
2023-06-02 10:38:42 +0100

    Add unit tests for api controllers and models

--- Gemfile
+++ Gemfile
@@ -43,7 +43,6 @@ group :development, :test do
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'factory_bot_rails'
   gem 'rspec-rails', '~> 6.0.0'
-  gem 'rswag-specs'
 end
 
 group :development do
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
-RSpec.describe Car, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'Cars', type: :model do
+  subject { Car.new(name: 'Lexus', brand: 'toyota', image: 'wwww.sample-image.com', lending_fee: 20.0, description: 'a smart and durable car') }
+
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'brand should be present' do
+    subject.brand = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'image link should be present' do
+    subject.image = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'lending fee should be present' do
+    subject.lending_fee = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'description should be present' do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'name should not be too short' do
+    subject.name = 'a'
+    expect(subject).to_not be_valid
+  end
+
+  it 'name should not be too long' do
+    subject.name = 'a' * 30
+    expect(subject).to_not be_valid
+  end
+
+  it 'brand name should not be too short' do
+    subject.brand = 'a'
+    expect(subject).to_not be_valid
+  end
+
+  it 'brand name should not be too long' do
+    subject.brand = 'a' * 25
+    expect(subject).to_not be_valid
+  end
+
+  it 'description should not be too short' do
+    subject.description = 'a'
+    expect(subject).to_not be_valid
+  end
+
+  it 'description should not be too long' do
+    subject.description = 'a' * 300
+    expect(subject).to_not be_valid
+  end
+
+  it 'appartment fee should allow valid values' do
+    subject.lending_fee = 100.0
+    expect(subject).to_not be_valid
+  end
 end
